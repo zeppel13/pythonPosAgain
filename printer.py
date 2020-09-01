2e2ac1cde415b7aa284b6e66492416aaf6521454
Sebastian <mail@sebastiankind.de>
2020-09-02 00:03:48 +0200

    doit.sh

--- printer.py
+++ printer.py
@@ -1,29 +1,22 @@
+#
 import time
-
+import sys
 from escpos.printer import Usb
 from PIL import Image
-im = Image.open("seb.jpg")
+# file printer
 
-#Epson = Usb(0x04b8,0x0202)
-zj = Usb(0x0416,0x5011)
+zj = Usb(0x0416, 0x5011)
 zj.textln("See chill")
-#zj.barcode("12345678", "EAN8")
-impls = ["bitImageRaster", "bitImageColumns"]
-#for imp in impls:
-#    time.sleep(1)
-#    zj.image(im, center=True, impl=imp)
-long_string = """
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
-Meine Mutter kann deine Mutter nicht kennen. Eureka (eure.ch) Protonmail lolol GPG
 
-"""
-#for i in range(1,5):
-#    zj.textln("size = {:}".format(i))
-#    zj.qr(long_string, size=i)
 
-#zj.soft_barcode("ean8", "13375005", impl="bitImageRaster")
+def printThis(val):
+    im = Image.open(val)
+    time.sleep(0.4)
+    zj.image(im, impl="bitImageRaster")
+
+
+for val in sys.argv[1:]:
+    print(val)
+    printThis(val)
+
+
